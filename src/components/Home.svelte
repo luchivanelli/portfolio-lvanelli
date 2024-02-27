8b1c1d679aec7096065cf0eb62a88be74d277f65
Luciana <lucianavanelli13gmail.com>
2024-02-27 13:37:02 -0300

    commit

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -5,7 +5,7 @@
 <section class="flex flex-col justify-center items-center m-16 gap-5" id="home">
     <div class="lg:flex gap-6 items-center">
         <h2 class="text-3xl font-semibold lg:text-end lg:text-5xl dark:text-white">Hello! 👋 <br> My name is <b class="font-bold tracking-wide">Luciana</b></h2>
-        <h1 class="text-3xl m-0 bg-gradient-to-r from-violeta via-azul to-celeste text-transparent bg-clip-text font-extrabold lg:text-5xl lg:border-l-2 border-violeta lg:pl-5">Frontend <br> Web Developer</h1>
+        <h1 class="text-4xl m-0 bg-gradient-to-r from-violeta via-azul to-celeste text-transparent bg-clip-text font-extrabold lg:text-5xl lg:border-l-2 border-violeta lg:pl-5">Frontend <br> Web Developer</h1>
     </div>
     <div class="lg:hidden">
         <div class="w-44 h-44 grid place-content-center rounded-full bg-gradient-to-l from-violeta via-azul to-celeste">
--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <div class="text-center flex justify-between lg:justify-evenly items-center px-5 border-b-2 h-20">
-    <h1 class="font-medium lg:text-4xl dark:text-white"><b class="bg-gradient-to-b from-violeta via-azul to-celeste text-transparent bg-clip-text">LV</b>anelli</h1>
+    <h1 class="font-medium text-3xl lg:text-4xl dark:text-white"><b class="bg-gradient-to-b from-violeta via-azul to-celeste text-transparent bg-clip-text">LV</b>anelli</h1>
     <button class="lg:hidden absolute right-16 text-black p-1 border-2 border-black rounded-full cursor-pointer hover:text-white hover:bg-black transition-colors dark:border-white dark:text-white dark:hover:bg-white dark:hover:text-black" on:click={handleMode}>
         <svg id="button-dark" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon hidden" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" /></svg>
         <svg id="button-light" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun-high" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656z" /><path d="M6.343 17.657l-1.414 1.414" /><path d="M6.343 6.343l-1.414 -1.414" /><path d="M17.657 6.343l1.414 -1.414" /><path d="M17.657 17.657l1.414 1.414" /><path d="M4 12h-2" /><path d="M12 4v-2" /><path d="M20 12h2" /><path d="M12 20v2" /></svg>
@@ -37,7 +37,7 @@
 </div>
  
 <div id="drawer-navigation" class="lg:hidden fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-64 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
-    <h5 id="drawer-navigation-label" class="bg-gradient-to-t from-violeta via-azul to-celeste text-transparent bg-clip-text text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Menu</h5>
+    <h5 id="drawer-navigation-label" class="bg-gradient-to-t from-violeta via-azul to-celeste text-transparent bg-clip-text text-base font-semibold text-gray-500 uppercase dark:text-celeste">Menu</h5>
     <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white" >
     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
